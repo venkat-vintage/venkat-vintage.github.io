9ad6952338c50eec788d5d09f10e19258752d3a3
Prashanth <prashanth@clictoprint.com>
2022-07-11 10:23:41 +0530

    adding draft -3

---
+++ script/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#about").click(function (e) {
+        $('html, body').animate({
+            scrollTop: $("#about_us").offset().top
+        }, 500);
+    })
+
+    $("#debt_one").click(function (e) {
+        $('html, body').animate({
+            scrollTop: $("#debt").offset().top
+        }, 500);
+    })
+
+    $("#due_one").click(function (e) {
+        $('html, body').animate({
+            scrollTop: $("#due").offset().top
+        }, 500);
+        e.preventDefault()
+    });
+    $("#contact").click(function (e) {
+        $('html, body').animate({
+            scrollTop: $("#contact_us").offset().top
+        }, 500);
+        e.preventDefault()
+    });
+})
